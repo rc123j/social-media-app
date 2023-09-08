ba6ca4209969fa71a662f47521b15a1622410f0d
rahul chaudhary <rahul.2001chaudhary@gmail.com>
2023-09-09 01:57:21 +0530

    some changes

---
+++ src/components/Logout.jsx
@@ -0,0 +1,28 @@
+// import React from 'react'
+// import { GoogleOAuthProvider, GoogleLogout } from "@react-oauth/google";
+
+
+// const Logout = () => {
+//   return (
+//     <>
+//     <div className="shadow-2xl">
+            
+//             <GoogleOAuthProvider clientId= '609295418358-n5u8m3rebfle8rkafi4rn9u72c8cu8i6.apps.googleusercontent.com'>
+
+//               <GoogleLogout
+//                 onSuccess={(credentialResponse) => {
+//                   console.log(credentialResponse);
+//                 }}
+//                 onError={() => {
+//                   console.log("Login Failed");
+//                 }}
+//               />
+              
+//             </GoogleOAuthProvider>
+            
+//           </div>
+//     </>
+//   )
+// }
+
+// export default Logout
