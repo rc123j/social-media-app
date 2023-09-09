d4496d53404ef4edddaab723602b120b2a0fb276
rahul chaudhary <rahul.2001chaudhary@gmail.com>
2023-09-09 16:59:11 +0530

    another changes

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useState, useRef, useEffect} from 'react'
+import {HiMenu} from 'react-icons/ai'
+import { AiFillCloseCircle } from 'react-icons/ai'
+import {Link, Route, Routes} from 'react-router-dom'
+
+import {  Sidebar, UserProfile} from '../components'
+import Pins from './Pins'
+import {client} from '../client';
+import logo from '../assets/logo.png'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease out'></div>
   )
 }
 
